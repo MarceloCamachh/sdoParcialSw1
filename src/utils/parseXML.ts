e108f9e7e0ce1753002529181d81eb17f1c40163
MarceloCamachh <marcelocamachogutierrez3@gmail.com>
2025-06-10 15:00:30 -0400

    se borro archivos que no usamos

--- src/utils/parseXML.ts
+++
@@ -1,7 +0,0 @@
-import { xml2js } from "xml-js";
-
-export const parseXMLFile = async (file: File) => {
-  const text = await file.text();
-  const result = xml2js(text, { compact: true });
-  return result;
-};
